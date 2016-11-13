845db545b56a60e4b8dfff28c1fce157337a3d29
Dominik Baran <dominik.baran7@gmail.com>
2016-11-13 16:37:06 +0100

    Initial commit

---
+++ src/Collective learning/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Collective_learning
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var window = new RenderWindow(new VideoMode(1280u, 768u), "SFML Example");
+
+            window.Closed += (s, e) => window.Close();
+            window.SetVisible(true);
+            window.SetVerticalSyncEnabled(true);
+            window.SetFramerateLimit(60);
+            window.SetActive();
+
+
+            CircleShape cs = new CircleShape(100.0f);
+            cs.FillColor = Color.Green;
+
+            while (window.IsOpen)
+            {
+                // Dispatch events to work with native event loop
+                window.DispatchEvents();
+                window.Clear();
+                window.Draw(cs);
+                window.Display();
+            }
+        }
+    }
+}
